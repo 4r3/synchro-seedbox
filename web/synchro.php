b8eb6c9e774b1268327b10d35d13635b979b82c4
Schl1gue <ed.patout@gmail.com>
2014-05-11 22:00:03 +0200

    Update synchro.php

--- web/synchro.php
+++ web/synchro.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>Synchronisation</title>
-<link rel=stylesheet type="text/css" href="./style.css">
+<link rel=stylesheet type="text/css" href="./style-synchro.css">
 </head>
 
 <body>
