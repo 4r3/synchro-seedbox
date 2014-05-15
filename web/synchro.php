50307cd7ced2cb0e01c37f45e7288b8d8a98cd61
Schl1gue <ed.patout@gmail.com>
2014-05-15 18:45:40 +0200

    Update synchro.php

--- web/synchro.php
+++ web/synchro.php
@@ -3,10 +3,19 @@
 <head>
 <meta charset="UTF-8">
 <title>Synchronisation</title>
-<link rel=stylesheet type="text/css" href="./style.css">
+<link rel=stylesheet type="text/css" href="./style-synchro.css">
 </head>
 
 <body>
+<?php
+$lines = file('./speed.cfg');
+
+// display file line by line
+foreach($lines as $line_num => $line) {
+    echo htmlspecialchars($line)."<br />\n";
+}
+?>
+
 <section class="box widget synchro">
 	<!-- Titre du bloc !-->
 	<header class="header">
@@ -19,7 +28,7 @@
 			$file='/tmp/synchro'; // On vérifie que le script est lancé
 			
 			// Remplacer <user> par votre utilisateur linux
-			$log='/home/<user>/synchro/logs/sending.log';
+			$log='/home/admin/synchro/logs/sending.log';
 
 			if(file_exists($file))
 			{
@@ -91,7 +100,7 @@
 				$file='/tmp/synchro';  // On vérifie que le script est bien lancé
 				
 				// Remplacer <user> par votre utilisateur linux
-				$list='/home/<user>/synchro/logs/liste_fichiers';
+				$list='/home/admin/synchro/logs/liste_fichiers';
 				
 				// On affiche la liste des fichiers dans l'infobulle
 				if(file_exists($file))
@@ -125,7 +134,84 @@
 			}
 		?>
 	</div>
+	
 </section>
+
+<br>
+<br>
+
+<section class="box widget synchro">
+	<!-- Titre du bloc !-->
+	<header class="header">
+		<h2>Speed</h2>
+	</header>
+	
+
+	
+
+<form action = "synchro.php" method="post">
+Vitesse : <input type = "number" name = "speed11" value="256" style="width:50px"><br />
+
+<!-- PO active <input value="checked" type="checkbox" onclick="Change()" name="por" id="choix_1" />
+
+<div id="madiv"  style="visibility:hidden;">
+Vitesse PO : <input type = "number" name = "speed2" value="512" style="width:0px">
+Début : <input type="time" name="hor1" value="00:00" />
+Fin : <input type="time" name="hor2" value="06:00" />
+</div> -->
+
+
+<input type = "submit" value = "Valider"><br>
+<br>
+
+<?php
+// on teste la déclaration de nos variables
+if (isset($_POST['speed11'])) {
+	// on affiche nos résultats
+
+	$speed11=$_POST['speed11'];
+	$speed2=$_POST['speed2'];
+	$hor1=$_POST['hor1'];
+	$hor2=$_POST['hor2'];
+    $por=$_POST['por'];
+
+	echo $speed11;
+	echo "<br>";
+	echo $speed2;
+	echo "<br>";
+	echo $hor1;
+	echo "<br>";
+	echo $hor2;
+	echo "<br>";
+	echo $por;
+	
+	// 1 : on ouvre le fichier
+$monfichier = fopen('./speed.cfg', 'w');
+ 
+// 2 : on lit la première ligne du fichier
+fputs($monfichier, "speed1=\"$speed11\"\nspeed2=\"$speed2\"\nsp2time1=\"$hor1\"\nsp2time2=\"$hor2\"\n");
+
+
+	if ($por=='checked') {
+        fputs($monfichier, "sp2ena=\"1\"");
+	}
+	
+	else {
+        fputs($monfichier, "sp2ena=\"0\"");
+	}
+	
+ 
+// 3 : quand on a fini de l'utiliser, on ferme le fichier
+fclose($monfichier);
+
+}
+?>
+
+
+</section>
+
+
+
 
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
 
@@ -134,8 +220,23 @@
 	var auto_refresh = setInterval(
 	function ()
 	{
-		$('#synchro').load('./synchro.php #synchro');
+		$('#synchro').load('./synhro.php #synchro');
 	}, 1000); // refresh every 10000 milliseconds
+</script>
+
+<!-- PO -->
+
+<script type="text/javascript">
+function Change() {
+
+if ((document.getElementById('choix_1').checked)) {
+document.getElementById('madiv').style.visibility="visible";
+}
+else {
+document.getElementById('madiv').style.visibility="hidden";
+}
+}
+
 </script>
 </body>
 </html>
