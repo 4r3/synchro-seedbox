88e3c2ef319b3839ebd4401ca42110589c9a7c76
Schl1gue <ed.patout@gmail.com>
2014-11-11 12:55:08 +0100

    Update makeinstall.sh
    Ajout de la sécurisation de la page web

--- makeinstall.sh
+++ makeinstall.sh
@@ -75,8 +75,12 @@ echo -e "${CGREEN}Entrer le dossier de synchro sur le NAS\n(/volumeX/dossier sur
 read NASFOLDER
 echo ""
 
-echo -e "${CGREEN}Entrer le répertoire d'installation de la page web\n(/var/www):$CEND"
-read FOLDERWEB
+echo -e "${CGREEN}Entrer le nom d'utilisateur pour la page web:$CEND"
+read USERWEB
+echo ""
+
+echo -e "${CGREEN}Entrer le mot de passe pour la page web:$CEND"
+read PASSWEB
 echo ""
 
 #Création de l'arborescence du script
@@ -84,8 +88,15 @@ mkdir /home/$USER/synchro
 cp -R script/* /home/$USER/synchro
 
 #Création de l'arborescence de la page web
-mkdir $FOLDERWEB
-cp -R web/* $FOLDERWEB
+mkdir /var/www/syncnas
+cp -R web/* /var/www/syncnas
+chown -R www-data /var/www/syncnas
+
+#Création du vhost nginx
+cp ./syncnas.conf /etc/nginx/sites-enabled
+
+#Génération du .htpasswd
+htpasswd -cb /etc/nginx/passwd/syncnas_passwd $USERWEB $PASSWEB
 
 #Ecriture des variables dans le fichier de configuration
 sed -i "s/@user@/$USER/g;" /home/$USER/synchro/config/user.cfg
@@ -108,7 +119,7 @@ chmod +x /home/$USER/synchro/synchro.sh
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "* * * * * cd /home/$USER/synchro && ./synchro.sh > /dev/null" >> mycron
+#echo "* * * * * cd /home/$USER/synchro && ./synchro.sh > /dev/null" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
